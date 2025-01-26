b8a2ccb7f7f7600bc687f3fa7f859262f3bfbf3f
Portavion <156545430+Portavion@users.noreply.github.com>
2025-01-26 23:02:43 +0000

    fix: another one

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -44,7 +44,7 @@ export const Contact = (props) => {
                 name="contact"
                 data-netlify="true"
                 method="POST"
-                onSubmit="Submit"
+                onSubmit={handleSubmit}
               >
                 <input type="hidden" name="form-name" value="contact" />
                 <div className="row">
