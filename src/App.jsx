0130a528036a6e64874fd27e0fbb128cefc44f30
Portavion <156545430+Portavion@users.noreply.github.com>
2025-01-28 13:55:19 +0000

    fix: pixel event name

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
   useEffect(() => {
     setLandingPageData(JsonData);
-    window.fbq("track", "MyEvent");
+    window.fbq("track", "PageView");
   }, []);
 
   return (
